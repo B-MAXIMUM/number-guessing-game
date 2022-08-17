5ed2de77cdab8c72d1b155b800b4d5922e02e958
B-MAXIMUM <bgmundy5223@gmail.com>
2022-08-17 11:04:06 -0700

    adding menue items

--- number guessing game/Assets/scripts/Number.cs
+++ number guessing game/Assets/scripts/Number.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Number : MonoBehaviour
 {
+    private int guess;
+    private int minValue = 1;
+    private int maxValue = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("welcome to the sexy number guessing game");
+        Debug.Log("the number range is from" + minValue "and" + maxValue );
     }
 
     // Update is called once per frame
