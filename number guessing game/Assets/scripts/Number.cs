7c5d2547b6308f7dde2e32649a3855c384da6205
B-MAXIMUM <bgmundy5223@gmail.com>
2022-08-19 11:29:57 -0700

    replayability

--- number guessing game/Assets/scripts/Number.cs
+++ number guessing game/Assets/scripts/Number.cs
@@ -10,16 +10,29 @@ public class Number : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("welcome to the sexy number guessing game");
-        Debug.Log("the number range is from " + minValue + " and " + maxValue );
-        Debug.Log("i will guess a number and you say if it is higher or lower then : " +guess+ "");
-        Debug.Log("push arrow keys depending on higher(up arrow) or lower and enter if correct");
+       StartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        cum();
+    }
+    void StartGame()
+    {
+        guess = 5;
+        maxValue = 10;
+        minValue = 1;
+
+        Debug.Log("welcome to the sexy number guessing game");
+        Debug.Log("the number range is from " + minValue + " to " +maxValue+ "");
+        Debug.Log("i will guess a number and you say if it is higher or lower then : " +guess+ "");
+        Debug.Log("push arrow keys depending on higher(up arrow) or lower(down arrow) and enter if correct");
+        maxValue = maxValue+1;
+    }
+    void cum()
+    {
+         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             minValue = guess;
             guess = (maxValue + minValue) / 2;
@@ -30,6 +43,12 @@ public class Number : MonoBehaviour
             maxValue = guess;
             guess = (maxValue +minValue) / 2;
             Debug.Log("is it higher or lower or am i right: " + guess);
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("HAHAHA I am so smart it was " +guess+ " you dumass");
+            Debug.Log("");
+            StartGame();
         }
     }
 }
